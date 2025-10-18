875f63bcee8c18eef5db0c5db74def667083ff23
gevrest <149320260+gevrest@users.noreply.github.com>
2025-10-18 16:34:00 +0500

    Added sparks, smoke and sounds

--- Assets/Code/Objects/Bonfire.cs
+++ Assets/Code/Objects/Bonfire.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Bonfire : MonoBehaviour
+namespace Game
 {
-    // Start is called before the first frame update
-    void Start()
+    public sealed class Bonfire : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private ParticleSystem _particleSystem;
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private Light _bonfireLight;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Start()
+        {
+            Kindle();
+        }
+
+        public void Kindle()
+        {
+            _particleSystem.Play();
+            _audioSource.Play();
+            _bonfireLight.intensity = 1;
+        }
+
+        public void Extinguish()
+        {
+            _particleSystem.Stop();
+            _audioSource.Stop();
+            _bonfireLight.intensity = 0;
+        }
     }
 }
